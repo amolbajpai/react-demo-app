0c35fb3ffc4c1a7cb6b7e9c5df15e2360033c071
Amol Bajpai <amolbajpai20@gmail.com>
2024-12-14 22:12:50 +0530

    first commit

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,6 @@
+// PascalCasing
+function Message() {
+    return <h1>Jai Shri Ram</h1>;
+}
+
+export default Message;
---
+++ readme.md
@@ -0,0 +1,5 @@
+npm create vite@4.1.0
+
+cd react-app
+npm install
+npm run dev
